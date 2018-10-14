7f6b476dcbd80c6682e6b30e7c2d0cb88533cdbb
ElenaMetelitsa <el_met@list.ru>
2018-10-14 21:51:12 +0300

    Tutorial: 2. add hightscores list

--- StarExplorer/game.lua
+++ StarExplorer/game.lua
@@ -164,7 +164,8 @@ local function restoreShip()
 end
 
 local function endGame ()
-  composer.gotoScene( "menu", { time=800, effect="crossFade" } )
+  composer.setVariable( "finalScore", score )
+  composer.gotoScene( "highscores", { time=800, effect="crossFade" } )
 end
 
 local function onCollision( event )
